d34ea31bec067258ee73800c006ddb2be41981eb
Afzal Afzal <142334300+afzal94551@users.noreply.github.com>
2024-07-06 05:09:45 +0530

    Update Country.sql

--- Country.sql
+++ Country.sql
@@ -1,12 +1,12 @@
-INSERT Country (Sr_No, Country) VALUES (1, 'India')
+INSERT into Country (Sr_No, Country) VALUES (1, 'India')
 ;
-INSERT Country (Sr_No, Country) VALUES (2, 'Malaysia')
+INSERT into Country (Sr_No, Country) VALUES (2, 'Malaysia')
 ;
-INSERT Country (Sr_No, Country) VALUES (3, 'Japa')
+INSERT into Country (Sr_No, Country) VALUES (3, 'Japa')
 ;
-INSERT Country (Sr_No, Country) VALUES (4, 'China')
+INSERT into Country (Sr_No, Country) VALUES (4, 'China')
 ;
-INSERT Country (Sr_No, Country) VALUES (5, 'Russia')
+INSERT into Country (Sr_No, Country) VALUES (5, 'Russia')
 ;
-INSERT Country (Sr_No, Country) VALUES (6, 'Singapur')
+INSERT into Country (Sr_No, Country) VALUES (6, 'Singapur')
 ;
