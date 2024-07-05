48067ed8a0f396a8f1c5f64ce47a4af9893b2dc9
Afzal Afzal <142334300+afzal94551@users.noreply.github.com>
2024-07-06 05:19:03 +0530

    Update Customer Segment.sql

--- Customer Segment.sql
+++ Customer Segment.sql
@@ -1,6 +1,6 @@
-INSERT Customer_Segment (ID, Segment) VALUES (1, 'Consumer')
+INSERT into Customer_Segment (ID, Segment) VALUES (1, 'Consumer')
 ;
-INSERT Customer_Segment (ID, Segment) VALUES (2, 'Corporate')
+INSERT into Customer_Segment (ID, Segment) VALUES (2, 'Corporate')
 ;
-INSERT Customer_Segment (ID, Segment) VALUES (3, 'B2B')
+INSERT into Customer_Segment (ID, Segment) VALUES (3, 'B2B')
 ;
