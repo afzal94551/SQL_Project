da07a7e9cfeb4958e61eef4b39af5aa5b911968d
Afzal Afzal <142334300+afzal94551@users.noreply.github.com>
2024-07-06 05:29:25 +0530

    Update Product.sql

--- Product.sql
+++ Product.sql
@@ -1,50 +1,49 @@
-INSERT Product (Product_ID, Product, Model) VALUES ('DE-e1-16', 'Dektop', 'Game1')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('DE-i1-18', 'Dektop', 'Super_i1')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('DE-n1-13', 'Dektop', 'Gen1')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('DE-n2-14', 'Dektop', 'Gen2')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('DE-n3-15', 'Dektop', 'Gen3')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('DE-ra-17', 'Dektop', 'GameUltra')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('LA-A1-6', 'Laptop', 'GAME_ULTRA1')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('LA-A3-7', 'Laptop', 'GAME_ULTRA3')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('LA-A5-8', 'Laptop', 'GAME_ULTRA5')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('LA-i1-4', 'Laptop', 'R2Ri1')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('LA-i3-5', 'Laptop', 'R2Ri3')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('LA-R2-1', 'Laptop', 'C22R2')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('LA-R3-2', 'Laptop', 'C22R3')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('LA-R4-3', 'Laptop', 'C22R4')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('MO-i1-19', 'Mobile', 'Dymond i1')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('MO-i1-23', 'Mobile', 'Game i1')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('MO-i3-20', 'Mobile', 'Dymond i3')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('MO-i3-24', 'Mobile', 'Game i3')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('MO-i5-21', 'Mobile', 'Dymond i5')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('MO-i5-25', 'Mobile', 'Game i5')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('MO-i7-22', 'Mobile', 'Dymond i7')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('TA-i1-9', 'Tab', 'i1')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('TA-i3-10', 'Tab', 'i3')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('TA-i5-11', 'Tab', 'i5')
-;
-INSERT Product (Product_ID, Product, Model) VALUES ('TA-i7-12', 'Tab', 'i7')
-;
+INSERT into Product (Product_ID, Product, Model) VALUES ('DE-e1-16', 'Dektop', 'Game1');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('DE-i1-18', 'Dektop', 'Super_i1');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('DE-n1-13', 'Dektop', 'Gen1');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('DE-n2-14', 'Dektop', 'Gen2');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('DE-n3-15', 'Dektop', 'Gen3');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('DE-ra-17', 'Dektop', 'GameUltra');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('LA-A1-6', 'Laptop', 'GAME_ULTRA1');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('LA-A3-7', 'Laptop', 'GAME_ULTRA3');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('LA-A5-8', 'Laptop', 'GAME_ULTRA5');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('LA-i1-4', 'Laptop', 'R2Ri1');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('LA-i3-5', 'Laptop', 'R2Ri3');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('LA-R2-1', 'Laptop', 'C22R2');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('LA-R3-2', 'Laptop', 'C22R3');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('LA-R4-3', 'Laptop', 'C22R4');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('MO-i1-19', 'Mobile', 'Dymond i1');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('MO-i1-23', 'Mobile', 'Game i1');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('MO-i3-20', 'Mobile', 'Dymond i3');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('MO-i3-24', 'Mobile', 'Game i3');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('MO-i5-21', 'Mobile', 'Dymond i5');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('MO-i5-25', 'Mobile', 'Game i5');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('MO-i7-22', 'Mobile', 'Dymond i7');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('TA-i1-9', 'Tab', 'i1');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('TA-i3-10', 'Tab', 'i3');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('TA-i5-11', 'Tab', 'i5');
+
+INSERT into Product (Product_ID, Product, Model) VALUES ('TA-i7-12', 'Tab', 'i7');
