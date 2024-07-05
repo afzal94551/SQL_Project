56bbb97dafc1120319b12e0e8d0837c7b73a393e
Afzal Afzal <142334300+afzal94551@users.noreply.github.com>
2024-07-06 04:55:09 +0530

    Update City.sql

--- City.sql
+++ City.sql
@@ -1,49 +1,49 @@
 
-INSERT City (ID, City) VALUES (1, 'Delhi')
-;
-INSERT City (ID, City) VALUES (2, 'Kuala Lumpur')
-;
-INSERT City (ID, City) VALUES (3, 'Nagoya')
-;
-INSERT City (ID, City) VALUES (4, 'Shanghai')
-;
-INSERT City (ID, City) VALUES (5, 'Chennai')
-;
-INSERT City (ID, City) VALUES (6, 'Osaka')
-;
-INSERT City (ID, City) VALUES (7, 'Beijing')
-;
-INSERT City (ID, City) VALUES (8, 'Moscow')
-;
-INSERT City (ID, City) VALUES (9, 'Hyderabad')
-;
-INSERT City (ID, City) VALUES (10, 'Gurgao')
-;
-INSERT City (ID, City) VALUES (11, 'Saint Petersburg')
-;
-INSERT City (ID, City) VALUES (12, 'Tokyo')
-;
-INSERT City (ID, City) VALUES (13, 'Bangalore')
-;
-INSERT City (ID, City) VALUES (14, 'East Region, Singapore')
-;
-INSERT City (ID, City) VALUES (15, 'George Tow')
-;
-INSERT City (ID, City) VALUES (16, 'Wuha')
-;
-INSERT City (ID, City) VALUES (17, 'Kobe')
-;
-INSERT City (ID, City) VALUES (18, 'Tengah')
-;
-INSERT City (ID, City) VALUES (19, 'Johor Bahru')
-;
-INSERT City (ID, City) VALUES (20, 'Mumbai')
-;
-INSERT City (ID, City) VALUES (21, 'Ipoh')
-;
-INSERT City (ID, City) VALUES (22, 'Noida')
-;
-INSERT City (ID, City) VALUES (23, 'Kaza')
-;
-INSERT City (ID, City) VALUES (24, 'Tampines')
-;
+INSERT Into City (ID, City) VALUES (1, 'Delhi');
+
+INSERT Into City (ID, City) VALUES (2, 'Kuala Lumpur');
+
+INSERT Into City (ID, City) VALUES (3, 'Nagoya');
+
+INSERT Into City (ID, City) VALUES (4, 'Shanghai');
+
+INSERT Into City (ID, City) VALUES (5, 'Chennai');
+
+INSERT Into City (ID, City) VALUES (6, 'Osaka');
+
+INSERT Into City (ID, City) VALUES (7, 'Beijing');
+
+INSERT Into City (ID, City) VALUES (8, 'Moscow');
+
+INSERT Into City (ID, City) VALUES (9, 'Hyderabad');
+
+INSERT Into City (ID, City) VALUES (10, 'Gurgao');
+
+INSERT Into City (ID, City) VALUES (11, 'Saint Petersburg');
+
+INSERT Into City (ID, City) VALUES (12, 'Tokyo');
+
+INSERT Into City (ID, City) VALUES (13, 'Bangalore');
+
+INSERT Into City (ID, City) VALUES (14, 'East Region, Singapore');
+
+INSERT Into City (ID, City) VALUES (15, 'George Tow');
+
+INSERT Into City (ID, City) VALUES (16, 'Wuha');
+
+INSERT Into City (ID, City) VALUES (17, 'Kobe');
+
+INSERT Into City (ID, City) VALUES (18, 'Tengah');
+
+INSERT Into City (ID, City) VALUES (19, 'Johor Bahru');
+
+INSERT Into City (ID, City) VALUES (20, 'Mumbai');
+
+INSERT Into City (ID, City) VALUES (21, 'Ipoh');
+
+INSERT Into City (ID, City) VALUES (22, 'Noida');
+
+INSERT Into City (ID, City) VALUES (23, 'Kaza');
+
+INSERT Into City (ID, City) VALUES (24, 'Tampines');
+
