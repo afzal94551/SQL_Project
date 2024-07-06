758fcf09a070f82f4222e9371d786f7526c07356
Afzal Afzal <142334300+afzal94551@users.noreply.github.com>
2024-07-06 05:57:51 +0530

    Update Ship_Mode.sql

--- Ship_Mode.sql
+++ Ship_Mode.sql
@@ -1,17 +1,4 @@
-
 INSERT into Ship_Mode (ID, Ship_Mode) VALUES (1, 'Road');
-
 INSERT into Ship_Mode (ID, Ship_Mode) VALUES (2, 'Air');
-
 INSERT into Ship_Mode (ID, Ship_Mode) VALUES (3, 'Ship');
-
 INSERT into Ship_Mode (ID, Ship_Mode) VALUES (4, 'Train');
-
-INSERT into Ship_Mode (ID, Ship_Mode) VALUES (1, 'Road');
-
-INSERT into Ship_Mode (ID, Ship_Mode) VALUES (2, 'Air');
-
-INSERT into Ship_Mode (ID, Ship_Mode) VALUES (3, 'Ship');
-
-INSERT into Ship_Mode (ID, Ship_Mode) VALUES (4, 'Train');
-
